25baaeb5d08cf369d5098e0a57093dd0d35c39d5
skychocowhite <simon2000.wang@gmail.com>
2023-04-04 16:21:53 +0800

    Debug scale mode event

--- create-your-own/input.js
+++ create-your-own/input.js
@@ -294,12 +294,13 @@ function workspaceTouchMoveEvent(event) {
   }
   else if (event.touches.length === 2) {
     console.log("here");
-    console.log(curState);
     if (curState === States.SCALE_MODE) {
       let firstFingerPos = { x: event.touches[0].clientX, y: event.touches[0].clientY };
       let secondFingerPos = { x: event.touches[1].clientX, y: event.touches[1].clientY };
       let width = parseInt(clickedTarget.style.width.substring(0, clickedTarget.style.width.length - 2));
       let height = parseInt(clickedTarget.style.height.substring(0, clickedTarget.style.height.length - 2));
+
+      console.log("width: " + width + " height: " + height);
 
       let widthOffset = abs(firstFingerPos.x - secondFingerPos.x) - abs(prevFirstFingerPos.x - prevSecondFingerPos.x);
       let heightOffset = abs(firstFingerPos.y - secondFingerPos.y) - abs(prevFirstFingerPos.y - prevSecondFingerPos.y);
